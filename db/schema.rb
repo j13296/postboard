8f8811d4205697ae6c237f756efcca44e2a5fa02
joanne <j13296@gmail.com>
2015-12-09 22:06:59 +0800

    created comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206093359) do
+ActiveRecord::Schema.define(version: 20151209134321) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
